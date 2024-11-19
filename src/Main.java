8ce853192fbfa2fef7da738995d99ad953b5d245
satyamanand11 <satyamanand11april@gmail.com>
2024-11-19 19:22:05 +0530

    Update Main.java
    Updated typo

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import service.*;
 
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         NotificationRepository repository = new NotificationRepositoryImpl();
